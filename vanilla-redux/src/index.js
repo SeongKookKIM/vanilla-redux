a85eec77641063c50b9961bdb5bd7c907b7b5aa1
SeongkookKIM <ksk8646@naver.com>
2023-04-20 12:01:03 +0900

    Vanilla-Redux 기초

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,10 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { legacy_createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const plus = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
+  return count;
+};
+
+const countStore = legacy_createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+plus.addEventListener("click", () => {
+  handleAdd();
+});
+minus.addEventListener("click", () => {
+  handleMinus();
+});
